b60561910bd96c40016427629b3c7c5e62da3588
Ayşe Kutrafalı <kutrafaliayse120@gmail.comgit>
2025-12-24 23:07:35 +0200

    Persist cleanings & shifts in PostgreSQL

--- server.js
+++ server.js
@@ -223,8 +223,13 @@ wss.on("connection", (ws) => {
 
 // ---------------------- SERVER START --------------------
 
-// ---------------------- SERVER START --------------------
-
-server.listen(PORT, "0.0.0.0", () => {
-    console.log(Server running on port ${ PORT });
-});
+initDB()
+    .then(() => {
+        server.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((e) => {
+        console.error("DB INIT ERROR:", e);
+        process.exit(1);
+    });
